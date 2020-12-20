429f2e83e734688b2e8d6302bb4e7b5b50fca5cb
pheeria <olzhas.askar@deliveryhero.com>
2020-12-20 01:53:29 +0100

    feat: seating system

--- 11-seating-system.py
+++ 11-seating-system.py
@@ -5,20 +5,20 @@ from pprint import pprint
 
 input = get_text('11')
 
-input = [
-  "L.LL.LL.LL",
-  "LLLLLLL.LL",
-  "L.L.L..L..",
-  "LLLL.LL.LL",
-  "L.LL.LL.LL",
-  "L.LLLLL.LL",
-  "..L.L.....",
-  "LLLLLLLLLL",
-  "L.LLLLLL.L",
-  "L.LLLLL.LL",
-]
+# input = [
+#   "L.LL.LL.LL",
+#   "LLLLLLL.LL",
+#   "L.L.L..L..",
+#   "LLLL.LL.LL",
+#   "L.LL.LL.LL",
+#   "L.LLLLL.LL",
+#   "..L.L.....",
+#   "LLLLLLLLLL",
+#   "L.LLLLLL.L",
+#   "L.LLLLL.LL",
+# ]
 
-def countOccurences(src, y, x, n):
+def count_occurences(src, y, x, n):
     result = 0
     height = len(src)
     width = len(src[0])
@@ -37,12 +37,12 @@ def countOccurences(src, y, x, n):
                 result += 1
     return result
 
-def countOccurences2(src, y, x, n):
+def count_occurences2(src, y, x, n):
     result = 0
     height = len(src)
     width = len(src[0])
     
-    for i in range(y, 0, -1):
+    for i in range(y - 1, -1, -1):
         cell = src[i][x]
         if cell == '.':
             pass
@@ -50,7 +50,7 @@ def countOccurences2(src, y, x, n):
             result = result + 1 if cell == n else result
             break
 
-    for i in range(y, height, 1):
+    for i in range(y + 1, height, 1):
         cell = src[i][x]
         if cell == '.':
             pass
@@ -58,7 +58,7 @@ def countOccurences2(src, y, x, n):
             result = result + 1 if cell == n else result
             break
 
-    for i in range(x, 0, -1):
+    for i in range(x - 1, -1, -1):
         cell = src[y][i]
         if cell == '.':
             pass
@@ -66,7 +66,7 @@ def countOccurences2(src, y, x, n):
             result = result + 1 if cell == n else result
             break
             
-    for i in range(x, width, 1):
+    for i in range(x + 1, width, 1):
         cell = src[y][i]
         if cell == '.':
             pass
@@ -74,6 +74,47 @@ def countOccurences2(src, y, x, n):
             result = result + 1 if cell == n else result
             break
 
+    topleft = min(y, x)
+    for i in range(1, topleft + 1):
+        cell = src[y - i][x - i]
+        if cell == '.':
+            pass
+        else:
+            result = result + 1 if cell == n else result
+            break
+
+    topright = min(y, width - x)
+    for i in range(1, topright + 1):
+        if y - i < 0 or x + i == width:
+            break
+        cell = src[y - i][x + i]
+        if cell == '.':
+            pass
+        else:
+            result = result + 1 if cell == n else result
+            break
+
+    bottomleft = min(height - y, x)
+    for i in range(1, bottomleft + 1):
+        if y + i == height or x - i < 0:
+            break
+        cell = src[y + i][x - i]
+        if cell == '.':
+            pass
+        else:
+            result = result + 1 if cell == n else result
+            break
+
+    bottomright = min(height - y, width - x)
+    for i in range(1, bottomright + 1):
+        if y + i == height or x + i == width:
+            break
+        cell = src[y + i][x + i]
+        if cell == '.':
+            pass
+        else:
+            result = result + 1 if cell == n else result
+            break
     return result
 
 def process(src, tolerance):
@@ -85,9 +126,9 @@ def process(src, tolerance):
         result.append("")
         for x in range(width):
             cell = src[y][x]
-            if cell == 'L' and countOccurences2(src, y, x, '#') == 0:
+            if cell == 'L' and count_occurences2(src, y, x, '#') == 0:
                 result[y] += '#'
-            elif cell == '#' and countOccurences2(src, y, x, '#') >= tolerance:
+            elif cell == '#' and count_occurences2(src, y, x, '#') >= tolerance:
                 result[y] += 'L'
             else:
                 result[y] += cell     
@@ -107,10 +148,28 @@ for line in last:
         if letter == '#':
             seats += 1
 print(seats)
-# sudoku = [
-#     "111",
-#     "111",
-#     "111",
-#     "111",
+
+
+# placements = [
+# '.......#.',
+# '...#.....',
+# '.#.......',
+# '.........',
+# '..#L....#',
+# '....#....',
+# '.........',
+# '#........',
+# '...#.....',
+# '.##.##.',
+# '#.#.#.#',
+# '##...##',
+# '...L...',
+# '##...##',
+# '#.#.#.#',
+# '.##.##.',
+    # '.............',
+    # '.L.L.#.#.#.#.',
+    # '.............',
 # ]
-# pprint(countOccurences(sudoku, 3, 2, "1"))
+
+# print(countOccurences2(placements, 4, 3, '#'))
