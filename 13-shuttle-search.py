522e308085e16213b27a9951250112cf73e83bea
pheeria <olzhas.askar@deliveryhero.com>
2020-12-25 19:28:55 +0100

    feat: finally, not fully understanding

--- 13-shuttle-search.py
+++ 13-shuttle-search.py
@@ -5,9 +5,9 @@ from utils import get_text
 input = get_text('13')
 # input = [
 #     '939',
-#     # '7,13,x,x,59,x,31,19'
-#     # '17,x,13,19'
-#     # '67,7,x,59,61'
+# #     # '7,13,x,x,59,x,31,19'
+# #     '17,x,13,19'
+# #     # '67,7,x,59,61'
 #     '1789,37,47,1889'
 # ]
 
@@ -27,31 +27,52 @@ print(earliest['id'] * (earliest['arrival'] - time))
 
 def closure(idx, num, sign):
     def f(x):
-        print(f"x = {x} idx = {idx * sign} num = {num}")
+        # print(f"x = {x} idx = {idx * sign} num = {num}")
         return (x + (idx * sign)) % num == 0
     return f
 
-checks = []
-largest = max(buses)
-index = original.index(str(largest))
+def brute_force():
+    checks = []
+    largest = max(buses)
+    index = original.index(str(largest))
 
-for i in range(len(original)):
-    if i == index or original[i] == 'x':
-        pass
-    else:
-        s = -1 if i < index else 1
-        n = int(original[i])
-        checks.append({
-            'num': n,
-            'func': closure(abs(index - i), n, s)
-        })
+    for i in range(len(original)):
+        if i == index or original[i] == 'x':
+            pass
+        else:
+            s = -1 if i < index else 1
+            n = int(original[i])
+            checks.append({
+                'num': n,
+                'func': closure(abs(index - i), n, s)
+            })
 
-found = False
-final = 100000000000265
-funcs = list(map(lambda x: x['func'], sorted(checks, key = lambda k: k['num'], reverse = True)))
+    found = False
+    # final = 100000000000265
+    final = 1889
+    funcs = list(map(lambda x: x['func'], sorted(checks, key = lambda k: k['num'], reverse = True)))
 
-while not found:
-    final += largest
-    found = all(map(lambda x: x(final), funcs))
-    
-print(final - index)
+    # largest = 6779227
+    while not found:
+        final += largest
+        found = all(map(lambda x: x(final), funcs))
+    print(final - index)
+
+def chinese_remainder():
+    factor = 1
+    time = 0
+    for index, bus in enumerate(original):
+        if bus == "x":
+            continue
+
+        while (time + index) % int(bus) > 0:
+            time += factor
+
+        factor *= int(bus)
+
+        print(index, bus, time, factor)
+
+
+    print(time)
+
+chinese_remainder()
