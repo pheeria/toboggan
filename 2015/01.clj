5053f2c24330417c4863230b2b9e14e5a85d78b8
Olzhas Askar <olzhas.askar@gmail.com>
2023-11-20 20:27:43 +0100

    feat: 2015 day one

---
+++ 2015/01.clj
@@ -0,0 +1,32 @@
+(use '[clojure.string :refer trim])
+
+(def input (trim (slurp "01.txt")))
+
+(defn part-one [stream]
+  (loop [counter 0
+         data stream]
+    (if (empty? data)
+      counter
+      (if (= (first data) \()
+           (recur (inc counter) (rest data))
+           (recur (dec counter) (rest data))))))
+
+(defn part-one-alternative [stream]
+  (let [parens (frequencies stream)]
+    (- (get parens \() (get parens \)))))
+
+
+(defn part-two [stream]
+  (loop [counter 0
+         position 0
+         data stream]
+    (if (= counter -1)
+      position
+      (if (= (first data) \()
+           (recur (inc counter) (inc position) (rest data))
+           (recur (dec counter) (inc position) (rest data))))))
+
+
+(part-one input)
+(part-one part-one-alternative)
+(part-two input)
