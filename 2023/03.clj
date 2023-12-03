e6d44fc71968bde91d7bc2249bb2021d03cc701d
Olzhas Askar <olzhas.askar@gmail.com>
2023-12-04 00:52:44 +0100

    feat: part two

--- 2023/03.clj
+++ 2023/03.clj
@@ -1,5 +1,4 @@
-(require '[clojure.string :as str]
-         '[clojure.pprint :refer [pprint]])
+(require '[clojure.string :as str])
 
 (def input
   (-> "03.txt"
@@ -7,42 +6,41 @@
       str/trim
       str/split-lines))
 
-(defn next-number-pair [pair]
+(defn next-number [pair]
   (let [processed (->> pair
                        (drop-while #(not (Character/isDigit (last %))))
                        (take-while #(Character/isDigit (last %))))
-        start (ffirst processed)
-        end   (first (last processed))
         number (->> processed
                     (map last)
                     (apply str))]
     (if (empty? number)
       {}
-      {:number (Integer/parseInt number) :start start :end end})))
+      {:number (Integer/parseInt number)
+       :start (ffirst processed)
+       :end (first (last processed))})))
 
-(defn parse-line [text]
+(defn parse-numbers [text]
   (loop [line (map-indexed vector text)
          numbers []]
-    (let [number (next-number-pair line)]
+    (let [number (next-number line)]
       (if (empty? number)
         numbers
         (let [new-index (inc (:end number))
-              new-numbers (conj numbers number)
-              new-line (drop-while #(<= (first %) new-index) line)]
+              new-line (drop-while #(<= (first %) new-index) line)
+              new-numbers (conj numbers number)]
         (recur new-line new-numbers))))))
-
-(defn val-at [stream row col]
-  (-> stream
-      (nth row)
-      (nth col)))
 
 (defn constraints [coord max-coord]
   [(max 0 (dec coord))
    coord
    (min (dec max-coord) (inc coord))])
 
-(defn valid-char? [ch]
-  (not (or (Character/isDigit ch) (= ch \.))))
+(defn valid-char? [stream row col]
+  (let [ch (-> stream
+               (nth row)
+               (nth col))]
+    (not (or (Character/isDigit ch)
+             (= ch \.)))))
 
 (defn valid-number? [stream row number]
   (let [maxrows (count stream)
@@ -56,14 +54,14 @@
           0
           (let [[x-1 x x+1] (constraints col maxcols)]
             (recur (inc col)
-                   (or (valid-char? (val-at stream y-1 x-1))
-                       (valid-char? (val-at stream y-1 x))
-                       (valid-char? (val-at stream y-1 x+1))
-                       (valid-char? (val-at stream y x-1))
-                       (valid-char? (val-at stream y x+1))
-                       (valid-char? (val-at stream y+1 x-1))
-                       (valid-char? (val-at stream y+1 x))
-                       (valid-char? (val-at stream y+1 x+1))))))))))
+                   (or (valid-char? stream y-1 x-1)
+                       (valid-char? stream y-1 x)
+                       (valid-char? stream y-1 x+1)
+                       (valid-char? stream y x-1)
+                       (valid-char? stream y x+1)
+                       (valid-char? stream y+1 x-1)
+                       (valid-char? stream y+1 x)
+                       (valid-char? stream y+1 x+1)))))))))
 
 (defn valid-line? [stream row numbers]
   (->>
@@ -71,61 +69,40 @@
     (map #(valid-number? stream row %))
     (apply +)))
 
+(defn parse-stars [row text]
+  (->> text
+       (map-indexed vector)
+       (filter #(= (last %) \*))
+       (map #(hash-map :row row :col (first %)))))
+
+(defn valid-star? [numbers star]
+  (let [col (:col star)
+        [y-1 y y+1] (constraints (:row star) (count numbers))
+        all (concat (nth numbers y-1)
+                    (nth numbers y)
+                    (nth numbers y+1))
+        filtered (->> all
+                      (filter #(and (>= col (dec (:start %)))
+                                    (<= col (inc (:end %)))))
+                      (map :number))]
+    (if (= (count filtered) 2)
+      (apply * filtered)
+      0)))
+
 (defn part-one [stream]
   (->> stream
-       (map-indexed #(hash-map :index %1 :numbers (parse-line %2)))
+       (map-indexed #(hash-map :index %1 :numbers (parse-numbers %2)))
        (remove #(empty? (:numbers %)))
        (map #(valid-line? stream (:index %) (:numbers %)))
        (apply +)))
 
 (defn part-two [stream]
-
-)
+  (let [numbers (map parse-numbers stream)]
+    (->> stream
+         (map-indexed #(parse-stars %1 %2))
+         flatten
+         (map #(valid-star? numbers %))
+         (apply +))))
 
 (println (part-one input))
 (println (part-two input))
-
- 
-(parse-line "..35..633.")
-
-(valid-line? [
-"467..114.."
-"...*......"
-"..35..633."
-"......#..."
-"617*......"
-".....+.58."
-"..592....."
-"......755."
-"...$.*...."
-".664.598.."
-] 0 [{:number 467, :start 0, :end 2} {:number 114, :start 5, :end 7}])
-
-(count [
-"467..114.."
-"...*......"
-"..35..633."
-"......#..."
-"617*......"
-".....+.58."
-"..592....."
-"......755."
-"...$.*...."
-".664.598.."
-])
-
-(part-one [
-"467..114.."
-"...*......"
-"..35..633."
-"......#..."
-"617*......"
-".....+.58."
-"..592....."
-"......755."
-"...$.*...."
-".664.598.."
-])
-(part-two "")
-
-
